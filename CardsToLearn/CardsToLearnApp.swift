e948e642d4e868ee3d39537084ed3460b5ea8e87
CaglayanCosgun <115452046+CaglayanCosgun@users.noreply.github.com>
2023-04-05 00:29:10 +0200

    Initial Commit

---
+++ CardsToLearn/CardsToLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsToLearnApp.swift
+//  CardsToLearn
+//
+//  Created by Cagla Yan on 05.04.23.
+//
+
+import SwiftUI
+
+@main
+struct CardsToLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
